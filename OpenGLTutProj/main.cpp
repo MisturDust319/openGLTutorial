946e00336a3e39657e31fed527d97d6bec3078c7
MisturDust319 <jarlofearls@gmail.com>
2016-04-04 00:39:05 -0400

    added mesh support

--- OpenGLTutProj/main.cpp
+++ OpenGLTutProj/main.cpp
@@ -3,6 +3,7 @@
 
 #include "display.h"
 #include "shader.h"
+#include "mesh.h"
 
 int main(int args, char** argv)
 {
@@ -10,9 +11,16 @@ int main(int args, char** argv)
 
 	Shader shader("./res/basicShader");
 
+	Vertex vertices[] = { Vertex(glm::vec3(-0.5, -0.5, 0)),
+							Vertex(glm::vec3(0, 0.5, 0)),
+							Vertex(glm::vec3(0.5, -0.5, 0)) };
+	Mesh mesh(vertices, sizeof(vertices) / sizeof(vertices[0]));
+
 	while (!display.IsClosed()) {
 		display.Clear(0.0f, 0.15f, 0.3f, 1.0f);
 		shader.Bind();
+
+		mesh.Draw();
 
 		display.Update();
 	}
